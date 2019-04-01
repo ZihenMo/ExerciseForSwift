7fc801024ea050a1eb3b4990744ab12677488130
muzimo <muzimo@gshopper.com>
2019-04-01 13:25:01 +0800

    Chore: Update the Pod Lib.

---
+++ ArchitecturalModel/ArchitecturalModel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitecturalModel
+//
+//  Created by gshopper on 2019/4/1.
+//  Copyright © 2019 mzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
+
